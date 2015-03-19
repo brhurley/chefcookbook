842900f06141393010c159b31a1475b758c27808
Brian Hurley <brhurley@gmail.com>
2015-03-19 18:09:21 -0500

    Added java8

---
+++ premiercode/metadata.rb
@@ -0,0 +1,7 @@
+name "elssetup"
+maintainer "Brian Hurley"
+maintainer_email "brian1@premiercodeinc.com"
+description "Chef recipes"
+long_description "Recipes for chef"
+version "1.0"
+recipe "java8", "Install latest Oracle Java"
