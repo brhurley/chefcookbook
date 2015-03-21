488464bd8ab32a4224a552e2177207fd90f8227f
Brian Hurley <brhurley@gmail.com>
2015-03-21 02:45:34 -0500

    wip

--- premiercode/recipes/java8.rb
+++ premiercode/recipes/java8.rb
@@ -1,6 +1,1 @@
-default['java']['jdk_version'] = '8'
-default['java']['install_flavor'] = 'oracle'
-default['java']['jdk']['8']['x86_64']['url'] = 'https://s3.amazonaws.com/ck-common-data/artifacts/jdk-8u25-linux-x64.tar.gz'
-default['java']['oracle']['accept_oracle_download_terms'] = true
-
 include_recipe 'java'
